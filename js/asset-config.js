16319fb3789a9de275ffbd28cc32a3fd7cbe86b7
CverinhaBr123 <39890180+CverinhaBr123@users.noreply.github.com>
2026-04-21 20:03:51 -0300

    Update game audio flow

--- js/asset-config.js
+++ js/asset-config.js
@@ -2,5 +2,10 @@ window.ECOS_ASSET_CONFIG = {
   enableCustomMenuBackground: true,
   customMenuBackground: 'assets/images/menu/fundo_inicial_custom.png',
   enableMenuMusic: true,
-  menuMusic: 'assets/audio/menu-theme.mp3'
+  menuMusic: 'assets/audio/menu-theme.mp3',
+  characterMusic: {
+    dwarf: '',
+    pony: '',
+    necromancer: ''
+  }
 };
